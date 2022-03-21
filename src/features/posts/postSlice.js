e52dc3f3d390ac8cab0e05e8c2ecd276249d687e
Jonas <boone.jonas@outlook.be>
2022-03-21 15:48:38 +0100

    refactor: added reducer for adding a new post

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -8,7 +8,10 @@ const initialState = [
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
-  reducers: {},
+  reducers: {
+    postAdded: (state, action) => state.push(action.payload),
+  },
 })
 
+export const { postAdded } = postsSlice.actions
 export const postsReducer = postsSlice.reducer
