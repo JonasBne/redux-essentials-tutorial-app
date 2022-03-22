a2d27fa9eba2682a71db3fa45a34678e29330ec8
Jonas <boone.jonas@outlook.be>
2022-03-22 09:42:58 +0100

    feat: added a component to display the author of a post

--- src/features/posts/postsList.js
+++ src/features/posts/postsList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { PostAuthor } from './PostAuthor'
 
 export const PostsList = () => {
   const posts = useSelector((state) => state.posts)
@@ -11,6 +12,7 @@ export const PostsList = () => {
       {posts.map((post) => (
         <article className="post-excerpt" key={post.id}>
           <h3>{post.title}</h3>
+          <PostAuthor userId={post.userId} />
           <p className="post-content">{post.content.substring(0, 100)}</p>
           <Link className="button muted-button" to={`/posts/${post.id}`}>
             View
